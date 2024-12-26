cd5dd9d3eb2125b150bcaa3ab741fe399c0ff112
Sudhagar <sudhaind@gmail.com>
2024-12-26 13:15:13 -0800

    added chrome options in BasePage class

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -39,12 +40,18 @@ public Properties p;
 		
 		logger =LogManager.getLogger(this.getClass());
 		//I put the information.
+		
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		//WebDriver driver = new ChromeDriver(options);
+		
 		System.setProperty("webdriver.chrome.driver", "C:\\Software_JARs\\chromedriver-win32\\chromedriver.exe");
 		
 		switch(br.toLowerCase())
 		
 		{
-		case "chrome" :driver = new ChromeDriver();break;
+		case "chrome" :driver = new ChromeDriver(options);break;
 		case "edge"   :driver =new EdgeDriver();break;
 		case "firefox": driver =new FirefoxDriver();break;
 		default:System.out.println("Invalid browser name..");return;
